726ad3923dcb183258a2b252759b7128fb05cfd0
Aminodin <aminakbari.young2021@gmail.com>
2022-12-24 13:29:09 +0330

    styling

--- __manifest__.py
+++ __manifest__.py
@@ -2,16 +2,16 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name' : 'SimpleTodoList',
-    'version' : '1.0',
+    'version' : '0.0.01',
     'summary': 'daily',
     'sequence': -101,
     'author':'Aminodin',
     'description': """
 
     """,
-    'category': 'Accounting/Edu',
+    'category': '',
     
-    'website': 'https://www.Google.com',
+    'website': 'https://aminodin.pythonanywhere.com/',
     'depends' : ['base'],
     'data': [
         'security/ir.model.access.csv',
